1eab37fc3c17013ede7586909752112b12d420b9
Hitesh Peddireddy <hitesh_peddireddy@epam.com>
2023-03-29 23:09:26 +0530

    This is my first project into local repository

---
+++ MyMVCApplication/MyMVCApplication/Controllers/SignupsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyMVCApplication.Entities;
+
+namespace MyMVCApplication.Controllers
+{
+    public class SignupsController : Controller
+    {
+        private SubmitEntities db = new SubmitEntities();
+
+        // GET: Signups
+        public ActionResult Index()
+        {
+            return View(db.Signups.ToList());
+        }
+
+        // GET: Signups/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Signup signup = db.Signups.Find(id);
+            if (signup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(signup);
+        }
+
+        // GET: Signups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Signups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Userid,FirstName,LastName,EmailId,PhoneNumber,Password")] Signup signup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Signups.Add(signup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(signup);
+        }
+
+        // GET: Signups/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Signup signup = db.Signups.Find(id);
+            if (signup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(signup);
+        }
+
+        // POST: Signups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Userid,FirstName,LastName,EmailId,PhoneNumber,Password")] Signup signup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(signup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(signup);
+        }
+
+        // GET: Signups/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Signup signup = db.Signups.Find(id);
+            if (signup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(signup);
+        }
+
+        // POST: Signups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Signup signup = db.Signups.Find(id);
+            db.Signups.Remove(signup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyMVCApplication/MyMVCApplication/Models/Submit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCApplication.Models
+{
+    public class Submit
+    {
+        [Required]
+        [EditorBrowsable]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public int PhoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string Occupation { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Feedback { get; set; }
+       
+    }
+}
